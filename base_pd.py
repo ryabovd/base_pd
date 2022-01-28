aa98bb30e9d7a05397f7a65089b31832f0310b3e
Dmitry Ryabov <ryabovd@outlook.com>
2022-01-28 17:09:36 +0700

    Add func write data

--- base_pd.py
+++ base_pd.py
@@ -3,7 +3,17 @@ import csv
 
 def main():
     base_file = "base_pd.csv"
+    base_structure = ['ФИО', 'Дата рождения yyyy.mm.dd', 'Место рождения', 'Паспорт (Номер, Кем выдан, Дата выдачи, Код подразделения)', 'СНИЛС', 'ИНН']
     base_file_read(base_file)
+    
+    n = int(input("Введите количество записей для ввода "))
+    data_list = []
+    for i in range(n):
+        data = []
+        for y in range(len(base_structure)):
+            data_input = input("Введите данные: " + base_structure[y] + " ")
+            data.append(data_input)
+        base_file_write(base_file, data)
 
 def base_file_read(base_file):
     with open(file=base_file, mode="r", encoding="UTF-8") as base:
@@ -11,6 +21,11 @@ def base_file_read(base_file):
         for row in reader:
             print(row)
 
+def base_file_write(base_file, data):
+    print(data)
+    with open(file=base_file, mode="a", encoding="UTF-8") as base:
+        writer = csv.writer(base, delimiter=';')
+        writer.writerow(data)
 
 if __name__ == "__main__":
     main()
