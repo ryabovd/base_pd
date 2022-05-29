c86120f3d6623aa6b2b17ff957789e62b97c8e4f
ryabovd <ryabovd@outlook.com>
2022-05-29 18:17:01 +0700

    %DATE_NOW

--- commit.bat
+++ commit.bat
@@ -1,9 +1,9 @@
 color B
-datetime_now=`%date%`
+set DATE_NOW=%date%
 git config --global user.name "ryabovd"
 git config --global user.email "ryabovd@outlook.com"
 git add *
-git commit -m "$datetime_now"
+git commit -m "%%DATE_NOW"
 git branch -M main
 git remote add origin https://github.com/ryabovd/base_pd.git
 git push -u origin main
