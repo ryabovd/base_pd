f96f13f5abed1fd2a0fad9d30dbb8723bfc13949
ryabovd <ryabovd@outlook.com>
2022-05-29 18:37:34 +0700

    29.05.2022

--- commit.bat
+++ commit.bat
@@ -5,8 +5,9 @@ pause
 git config --global user.name "ryabovd"
 git config --global user.email "ryabovd@outlook.com"
 git add *
-git commit -m $DATE_NOW
+git commit -m %date%
 git branch -M main
 git remote add origin https://github.com/ryabovd/base_pd.git
 git push -u origin main
+pause
 
